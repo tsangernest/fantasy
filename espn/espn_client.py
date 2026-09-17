b3e5180cc579b5ed8e3e52c855d5fa46ee7f3c8d
p14s <tsangernest@hotmail.com>
2026-09-16 19:22:06 -0500

    * reachout to espnapi

--- espn/espn_client.py
+++ espn/espn_client.py
@@ -3,21 +3,23 @@ import httpx
 from pprint import PrettyPrinter
 from starlette.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 
-pp = PrettyPrinter(indent=2, width=1)
+pp = PrettyPrinter(indent=2)
 
 
-NFL = "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/athletes"
+# NFL = "https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/athletes"
+NFL_INJURIES = "https://site.api.espn.com/apis/site/v2/sports/football/nfl"
+PARAMS = {"lang": "en", "region": "us"}
 
 
 async def get_info():
-    async with httpx.AsyncClient() as aclient:
+    async with httpx.AsyncClient(params=PARAMS) as aclient:
         response = await aclient.get(
-            url=f"{NFL}/3139477",
-            params={"lang": "en", "region": "us"},
+            url=f"{NFL_INJURIES}/injuries",
+            headers={"Accept": "application/json"},
         )
         if HTTP_200_OK == response.status_code:
-            pp.pprint(response.json())
             pp.pprint(response.json().keys())
+            breakpoint()
         return HTTP_404_NOT_FOUND
 
 asyncio.run(get_info())
